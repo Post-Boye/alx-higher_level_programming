496aba922d3a998b9a9765c79b667c3cf7f88898
Post-Boye <boyejustice64@gmail.com>
2023-03-10 07:51:29 -0800

    hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
-
 if __name__ == "__main__":
-    file = dir(hidden_4)
-    length = len(file)
-    for i in range(0, length):
-        if file[i][0:2] != "__":
-            print(file[i])
-
+    hiddlist = dir(hidden_4)
+    hiddlist.sort()
+    for i in hiddlist:
+        if not(i.startswith("__")):
+            print(i)
