e6633a0609ad5b08eee5ad5cb17ad3586eaffb77
Post-Boye <boyejustice64@gmail.com>
2023-03-13 12:58:23 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
     if idx < 0:
         return None
@@ -6,4 +7,3 @@ def element_at(my_list, idx):
         return None
     else:
         return my_list[idx]
-
