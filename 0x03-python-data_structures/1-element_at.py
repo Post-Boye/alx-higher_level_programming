3d7683d06df032c7c00b094d20ace6fb06a87b81
Post-Boye <boyejustice64@gmail.com>
2023-03-13 12:53:47 -0700

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-def replace_in_list(my_list, idx, element):
-    n = len(my_list)
+def element_at(my_list, idx):
     if idx < 0:
-        return my_list
-    elif idx > n - 1:
-        return my_list
+        return None
+    if idx >= len(my_list):
+        return None
     else:
-        my_list[idx] = element
-        return my_list 
+        return my_list[idx]
+
